4bde0835b93bfa3c9602e72c8486c03c43b2268b
Dipankar Dutta <dip@fb.com>
2021-02-14 21:12:35 +0000

    automated chkin

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import os
 
 import pandas as pd
+import sys
+import traceback
 import io
 from pandas.io import parsers
 from patterns import patterns
@@ -8,7 +10,7 @@ from flask import Flask, render_template, request, Response
 import yfinance as yf
 from symbols import symbols
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
-from helper import create_figure, getDailyDataSet
+from helper import create_figure, getDailyDataSet, sample_buy_rule, sample_sell_rule
 # Installation is complicated
 # brew install ta-lib - pip will give clang error
 # export ARCHFLAGS="-arch x86_64"; /usr/bin/python3 -m pip install --user --upgrade psutil
@@ -37,12 +39,112 @@ def chartTest():
     return Response(output.getvalue(), mimetype='image/png')
 
 
+@app.route('/screen')
+def Screen():
+    fig = create_figure()
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+    return Response(output.getvalue(), mimetype='image/png')
+
+
 @app.route('/sample')
-def getSample():
+def sample():
     symbol = request.args.get('symbol')
     reload = request.args.get('reload')
     data = getDailyDataSet(reload).get(symbol)
-    return "<div><p>Data</p><pre>{}</pre></div>".format(data.tail())
+    lastdata = dict(data.iloc[-1])
+    return """<div>
+                <p>Data</p>
+                <pre>{}</pre>
+                <p>Last data</p>
+                <pre style="width: 500px; white-space: pre-wrap">{} </pre>
+            </div >""".format(data.tail(), lastdata)
+
+
+@app.route('/backtest')
+def backtest():
+    try:
+        symbol = request.args.get('symbol')
+        entry_rule = request.args.get('entry_rule')
+        exit_rule = request.args.get('exit_rule')
+        input = {
+            "symbol": symbol,
+            "entry_rule": entry_rule,
+            "exit_rule": exit_rule
+        }
+        result = {
+            'summary': {},
+            'order_book': []
+        }
+        if (symbol and entry_rule and exit_rule):
+            df = getDailyDataSet().get(symbol)
+            pos = 0
+            num = 0
+            buy_price = 0
+            sell_price = 0
+            buy_date = ''
+            sell_date = ''
+            for i in df.index:
+                if sample_buy_rule(df, i) and pos == 0:
+                    pos = 1
+                    buy_price = df['Close'][i]
+                    buy_date = df['Date'][i]
+
+                elif sample_sell_rule(df, i) and pos == 1:
+                    pos = 0
+                    sell_price = df['Close'][i]
+                    sell_date = df['Date'][i]
+                    result['order_book'].append({
+                        'buy_price': buy_price,
+                        'sell_price': sell_price,
+                        'buy_date': buy_date,
+                        'sell_date': sell_date,
+                        'per_change': (sell_price/buy_price - 1)*100
+                    })
+                # Test Just open
+                if(i == df['Open'].count() - 1 and pos == 1):
+                    pos = 0
+                    sell_price = df['Close'][i]
+                    sell_date = df['Date'][i]
+                    result['order_book'].append({
+                        'buy_price': buy_price,
+                        'sell_price': sell_price,
+                        'buy_date': buy_date,
+                        'sell_date': sell_date,
+                        'per_change': (sell_price/buy_price - 1)*100
+                    })
+    except Exception as e:
+        # traceback.print_exception()
+        return {'error': str(e)}
+
+    gain = 0
+    count_gain = 0
+    max_gain = 0
+    loss = 0
+    count_loss = 0
+    max_loss = 0
+    totalR = 1
+    for oe in result['order_book']:
+        if(oe['per_change'] > 0):
+            gain += oe['per_change']
+            count_gain += 1
+            max_gain = max(max_gain, oe['per_change'])
+        else:
+            loss += oe['per_change']
+            count_loss += 1
+            max_loss = min(max_loss, oe['per_change'])
+        totalR = totalR*((i/100)+1)
+
+    result['summary'] = {
+        'gain': gain,
+        'count_gain': count_gain,
+        'loss': loss,
+        'count_loss': count_loss,
+        'max_gain': max_gain,
+        'max_loss': max_loss,
+        'totalR': totalR
+    }
+    return render_template('backtest.html', result=result, input=input, symbols=symbols)
 
 
 @app.route('/')
--- helper.py
+++ helper.py
@@ -3,11 +3,14 @@ import os
 import random
 from flask import Response
 from matplotlib.figure import Figure
+import numpy as np
 import pandas as pd
 import talib
 
 
 mCacheDailyDataSet = None
+all_range = [3, 4, 5, 6, 7, 8, 9, 10, 12, 13,
+             14, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 100, 120, 150, 200]
 
 
 def create_figure():
@@ -19,11 +22,7 @@ def create_figure():
     return fig
 
 
-ema_range = [5, 10, 15, 20, 25, 30, 50, 100, 200]
-sma_range = [5, 10, 15, 20, 25, 30, 50, 100, 200]
-
-
-def getDailyDataSet(reload: False):
+def getDailyDataSet(reload=False):
     global mCacheDailyDataSet
     if mCacheDailyDataSet and reload != "1":
         return mCacheDailyDataSet
@@ -32,13 +31,61 @@ def getDailyDataSet(reload: False):
     for filename in datafiles:
         symbol = filename.split('.')[0]
         df = pd.read_csv('datasets/daily/{}'.format(filename))
-        # write your own info here
-        for ema in ema_range:
-            df["ema_{}".format(ema)] = df["Close"].ewm(
-                span=ema, adjust=False).mean()
-        for sma in sma_range:
-            df["sma_{}".format(sma)] = df["Close"].rolling(sma).mean()
+        # Make lower case << Validated
+        df['open'] = np.round(df['Open'], 2)
+        df['close'] = np.round(df['Close'], 2)
+        df['high'] = np.round(df['High'], 2)
+        df['low'] = np.round(df['Low'], 2)
+        df['volume'] = np.round(df['Volume'], 2)
+        df.drop(columns=['Open', 'Close', "High", "Low", "Volume"])
+        # write your own info here << Validated
+        for range in all_range:
+            df["ema_{}".format(range)] = np.round(df["close"].ewm(
+                span=range, adjust=False).mean(), 2)
+            df["sma_{}".format(range)] = np.round(
+                df["close"].rolling(range).mean(), 2)
+            df["wma_{}".format(range)] = talib.WMA(
+                df["close"], timeperiod=range)
+
+        # validated
+
+        #macd and RSI
+        #df['macd'] = talib.MACD(df['close'].as_matrix())
+        df["macd_macd"], df["macd_macdsignal"], df["macd_macdhist"] = talib.MACD(
+            df.close, fastperiod=12, slowperiod=26, signalperiod=9)
+        df['rsi'] = talib.RSI(df['close'].values, 14)
+        df['rsi_6'] = talib.RSI(df['close'].values, 6)
+        df['rsi_12'] = talib.RSI(df['close'].values, 12)
+        df['rsi_18'] = talib.RSI(df['close'].values, 18)
+
+        # band
+        df['bb_up_5'],  df['bb_mid_5'],  df['bb_down_5'] = talib.BBANDS(
+            df['close'], timeperiod=5)
+        df['bb_up_15'], df['bb_mid_15'], df['bb_down_15'] = talib.BBANDS(
+            df['close'], timeperiod=15)
+        df['bb_up_60'], df['bb_mid_60'], df['bb_down_60'] = talib.BBANDS(
+            df['close'], timeperiod=60)
+
+        df['sar'] = talib.SAR(df['high'], df['low'],
+                              acceleration=0.02, maximum=0.2)
+
         # Please add extra line here.
         result[symbol] = df
     mCacheDailyDataSet = result
     return mCacheDailyDataSet
+
+
+def sample_buy_rule(df, i):
+    cmin = min(df['ema_3'][i], df['ema_5'][i], df['ema_8'][i],
+               df['ema_10'][i], df['ema_12'][i], df['ema_15'][i])
+    cmax = max(df['ema_30'][i], df['ema_35'][i], df['ema_40'][i],
+               df['ema_45'][i], df['ema_50'][i], df['ema_60'][i])
+    return cmin > cmax
+
+
+def sample_sell_rule(df, i):
+    cmin = min(df['ema_3'][i], df['ema_5'][i], df['ema_8'][i],
+               df['ema_10'][i], df['ema_12'][i], df['ema_15'][i])
+    cmax = max(df['ema_30'][i], df['ema_35'][i], df['ema_40'][i],
+               df['ema_45'][i], df['ema_50'][i], df['ema_60'][i])
+    return cmin < cmax
