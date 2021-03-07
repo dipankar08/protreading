da8f117b9af6e3bcd4afd9f531b0b332f763b3d6
Dipankar Dutta <dip@fb.com>
2021-03-07 13:03:09 +0000

    automated chkin

--- backend/src/config/rootConfig.py
+++ backend/src/config/rootConfig.py
@@ -3,4 +3,4 @@ from src.config.MyTypes import TCandleType
 from typing import List
 
 SUPPORTED_CANDLE: List[TCandleType] = [
-    TCandleType.MIN_5, TCandleType.MIN_15, TCandleType.DAY_1]
+    TCandleType.MIN_5, TCandleType.MIN_15, TCandleType.DAY_1, TCandleType.HOUR_1]
