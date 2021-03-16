7f8219c2495578fded888864f5fadbd87a9c0da0
Dipankar Dutta <dutta.dipankar08@gmail.com>
2021-03-16 23:52:08 +0000

    Update start_radis.sh

--- backend/start_radis.sh
+++ backend/start_radis.sh
@@ -1,9 +1,6 @@
-#!/bin/bash
-if [ ! -d redis-stable/src ]; then
-    curl -O http://download.redis.io/redis-stable.tar.gz
-    tar xvzf redis-stable.tar.gz
-    rm redis-stable.tar.gz
-fi
-cd redis-stable
-make
-src/redis-server
+# Please run the server as asymster service
+# Unbuntu:
+ sudo apt update
+sudo apt install redis-server
+sudo systemctl restart redis.service
+sudo systemctl status redis
