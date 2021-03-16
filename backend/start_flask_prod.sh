689c03f98a8d614f3c91bbbbd8e17a98fe263cdc
Dipankar Dutta <dutta.dipankar08@gmail.com>
2021-03-16 23:55:21 +0000

    Update start_flask_prod.sh

--- backend/start_flask_prod.sh
+++ backend/start_flask_prod.sh
@@ -1,3 +1,3 @@
 #flask run --host 0.0.0.0 -p 5000 --no-debugger --key=/etc/letsencrypt/live/rc1.grodok.com/privkey.pem --cert /etc/letsencrypt/live/rc1.grodok.com/cert.pem
 #gunicorn -w 4 -b 0.0.0.0:5000 app 
-gunicorn --certfile=/etc/letsencrypt/live/rc1.grodok.com/fullchain.pem  --keyfile=/etc/letsencrypt/live/rc1.grodok.com/privkey.pem -w 4 --timeout 500 --bind 0.0.0.0:5000 app
+gunicorn --certfile=/etc/letsencrypt/live/realtime.grodok.com/fullchain.pem  --keyfile=/etc/letsencrypt/live/realtime.grodok.com/privkey.pem -w 4 --timeout 500 --bind 0.0.0.0:5000 app
