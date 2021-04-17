0df8972f13da3ddeb8c3b519283556261670a538
Dipankar Dutta <dip@fb.com>
2021-04-17 15:00:48 +0100

    automated chkin

--- apps/trading121/src/appstate/CONST.ts
+++ apps/trading121/src/appstate/CONST.ts
@@ -3,5 +3,5 @@ export const CACHE_KEY_SUMMARY = "CACHE_KEY_SUMMARY";
 export const CACHE_KEY_POSITION = "CACHE_KEY_POSITION";
 
 export const SIMPLESTORE_ENDPOINT = "https://api.grodok.com";
-//export const PRO_TRADING_SERVER = "https://dev.api.grodok.com:5000";
-export const PRO_TRADING_SERVER = "http://10.0.2.2:5000"; // 10.0.2.2 is the host machine IP
+export const PRO_TRADING_SERVER = "https://dev.api.grodok.com:5000";
+//export const PRO_TRADING_SERVER = "http://10.0.2.2:5000"; // 10.0.2.2 is the host machine IP
--- apps/trading121/src/libs/dlog.ts
+++ apps/trading121/src/libs/dlog.ts
@@ -10,6 +10,13 @@ export namespace dlog {
     console.log(JSON.stringify(obj));
     console.log("=========================== DEBUG OBJECT END ======================");
   }
+  export function map(map: Map<any, any>) {
+    console.log("=========================== DEBUG OBJECT  START ======================");
+    for (let x of map.keys()) {
+      console.log(`${JSON.stringify(x)} ==> ${JSON.stringify(map.get(x))}`);
+    }
+    console.log("=========================== DEBUG OBJECT END ======================");
+  }
   export function ex(e: Error) {
     console.log(`[ERROR]${e.message}`);
     console.log(e.stack);
