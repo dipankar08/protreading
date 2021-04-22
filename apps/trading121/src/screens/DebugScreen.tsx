388bd9ebc98f87e63c23ca0e2296863db5b9b2e9
Dipankar Dutta <dip@fb.com>
2021-04-22 02:15:05 +0100

    automated chkin

--- apps/trading121/src/screens/DebugScreen.tsx
+++ apps/trading121/src/screens/DebugScreen.tsx
@@ -2,8 +2,6 @@ import React, { useContext } from "react";
 import { Component } from "react";
 import { DButton, DContainer, DContainerSafe, DLayoutCol, DText, ScreenHeader } from "../components/basic";
 import CodePush from "react-native-code-push";
-import Analytics from "appcenter-analytics";
-import Crashes from "appcenter-crashes";
 export class DebugScreen extends Component {
   componentDidMount() {
     //CodePush.sync({ installMode: CodePush.InstallMode.ON_NEXT_RESUME }, this.syncWithCodePush);
@@ -11,8 +9,8 @@ export class DebugScreen extends Component {
   }
 
   checkEnable = async () => {
-    const enabled = await Analytics.isEnabled();
-    console.log("analytics : " + enabled);
+    //const enabled = await Analytics.isEnabled();
+    // console.log("analytics : " + enabled);
   };
   syncWithCodePush = (status: any) => {
     console.log("sync with code push : " + status);
@@ -22,7 +20,7 @@ export class DebugScreen extends Component {
     // console.log(test.should.crash);
     // Analytics.trackEvent("ClickEvent");
     // throw new Error('My Test crash!');
-    Crashes.generateTestCrash();
+    //Crashes.generateTestCrash();
     // throw new Error('This is a test javascript crash!');
   };
   createJSCrash = () => {
@@ -30,7 +28,7 @@ export class DebugScreen extends Component {
     // console.log(test.should.crash);
     // Analytics.trackEvent("ClickEvent");
     // throw new Error('My Test crash!');
-    Crashes.generateTestCrash();
+    //Crashes.generateTestCrash();
     // throw new Error('This is a test javascript crash!');
   };
 
