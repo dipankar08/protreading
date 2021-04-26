eea8bdea962aed24f69d493e9c2d7b9b0455bf18
Dipankar Dutta <dip@fb.com>
2021-04-26 01:12:19 +0100

    automated chkin

--- apps/trading121/src/core/BootScreen.tsx
+++ apps/trading121/src/core/BootScreen.tsx
@@ -1,10 +1,12 @@
 // Splash screen or boot screen is important for loading the boot data.
-
+import React from "react";
 import { useContext, useEffect } from "react";
 import { STYLES } from "../components/styles";
 import { CoreStateContext } from "./CoreContext";
 import { useCoreApi } from "./useCoreApi";
 import { DButton, DContainer, DContainerSafe, DLayoutCol, DText, ScreenHeader } from "../components/basic";
+import { ActivityIndicator, StyleSheet, Text, View } from "react-native";
+import { colors } from "../styles/colors";
 
 // In this screen we are trying to import { CACHE_KEY_MARKET } from '../appstate/CONST';
 export const BootScreen = ({ navigation }: any) => {
@@ -12,14 +14,26 @@ export const BootScreen = ({ navigation }: any) => {
   const coreApi = useCoreApi();
   useEffect(() => {
     coreApi.doAppBoot(() => {
-      coreApi.navigateNext("BOOT", navigation);
+      setTimeout(() => {
+        coreApi.navigateNext("BOOT", navigation);
+      }, 1000);
     });
   }, []);
 
   return (
-    <DContainer style={{ backgroundColor: STYLES.APP_COLOR_PRIMARY, justifyContent: "center", alignItems: "center", paddingHorizontal: 40 }}>
-      <DText dark>Loading .....</DText>
-      {coreApi.error.length > 0 ? <DText dark>{coreApi.error}</DText> : <DText>Please wait....</DText>}
+    <DContainer
+      style={{
+        backgroundColor: STYLES.APP_COLOR_PRIMARY,
+        justifyContent: "center",
+        alignItems: "center",
+        paddingHorizontal: 40,
+        flex: 1,
+        flexDirection: "column",
+      }}
+    >
+      <ActivityIndicator size="small" color="white" />
+      <Text style={{ color: "white", marginTop: 14, textAlign: "center", fontWeight: "bold" }}>Loading....</Text>
+      <Text style={{ color: colors.white, marginTop: 10, textAlign: "center" }}>(Trying to load boot config)</Text>
     </DContainer>
   );
 };
--- apps/trading121/src/core/NUXScreen.tsx
+++ apps/trading121/src/core/NUXScreen.tsx
@@ -1,9 +1,39 @@
+import React from "react";
 import { useContext, useEffect } from "react";
 import { DButton, DContainer, DText } from "../components/basic";
 import { dlog } from "../libs/dlog";
 import { TProps } from "../screens/types";
 import { CoreStateContext } from "./CoreContext";
 import { useCoreApi } from "./useCoreApi";
+import { StyleSheet } from "react-native";
+import AppIntroSlider from "react-native-app-intro-slider";
+import { View, Text, Image } from "react-native";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+import { colors } from "../styles/colors";
+
+const slides = [
+  {
+    key: 1,
+    title: "Smart Market Research",
+    text: "With Trading50, We provided useful insights of the market",
+    image: require("../../assets/images/1.png"),
+    backgroundColor: colors.cyan400,
+  },
+  {
+    key: 2,
+    title: "Smart Portfolios",
+    text: "Other cool stuff",
+    image: require("../../assets/images/2.png"),
+    backgroundColor: colors.blue400,
+  },
+  {
+    key: 3,
+    title: "Alerts and Screens",
+    text: "Always keep notfied with useful alerts",
+    image: require("../../assets/images/3.png"),
+    backgroundColor: colors.green400,
+  },
+];
 
 // Sign up logic...
 export const NuxScreen = ({ navigation }: TProps) => {
@@ -22,9 +52,76 @@ export const NuxScreen = ({ navigation }: TProps) => {
     };
   }, []);
   return (
-    <DContainer>
-      <DText>NUX SCREEN</DText>
-      <DButton onPress={completeNux}>COMPLETE</DButton>
+    <DContainer style={styles.container}>
+      <AppIntroSlider
+        renderItem={({ item }) => {
+          return (
+            <View style={[styles.slide, { backgroundColor: item.backgroundColor }]}>
+              <Text style={styles.title}>{item.title}</Text>
+              <Image source={item.image} style={styles.img} />
+              <Text style={styles.text}>{item.text}</Text>
+            </View>
+          );
+        }}
+        data={slides}
+        //onDone={completeNux}
+        renderDoneButton={() => {
+          return (
+            <View style={styles.buttonCircle}>
+              <MaterialCommunityIcons name="check" color={colors.green200} size={24} />
+            </View>
+          );
+        }}
+        renderNextButton={() => {
+          return (
+            <View style={styles.buttonCircle}>
+              <MaterialCommunityIcons name="arrow-right" color={colors.white} size={24} />
+            </View>
+          );
+        }}
+      />
     </DContainer>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 0,
+    paddingHorizontal: 0,
+    margin: 0,
+  },
+  slide: {
+    flex: 1,
+    textAlign: "center",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "yellow",
+  },
+  title: {
+    fontWeight: "900",
+    fontSize: 30,
+    color: "white",
+    marginBottom: 30,
+  },
+  img: {
+    resizeMode: "contain",
+    backgroundColor: "transparent",
+    height: 250,
+  },
+  text: {
+    marginTop: 100,
+    color: "white",
+    fontSize: 15,
+    paddingHorizontal: 30,
+    textAlign: "center",
+  },
+  buttonCircle: {
+    width: 40,
+    height: 40,
+    backgroundColor: "rgba(0, 0, 0, .2)",
+    borderRadius: 20,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
