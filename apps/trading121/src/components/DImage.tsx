58947d90b71bd21e5ac60f5551a6a62d26124eb7
Dipankar Dutta <dip@fb.com>
2021-05-05 02:12:49 +0100

    automated chkin

--- apps/trading121/src/components/DImage.tsx
+++ apps/trading121/src/components/DImage.tsx
@@ -1,23 +1,24 @@
 import React from "react";
 import { ActivityIndicator, StyleSheet, Text, Image } from "react-native";
+import { STRINGS } from "../res/strings";
 import { TProps } from "../screens/types";
 import { DLayoutCol } from "./basic";
-import { DTextSubTitle } from "./DText";
+import { DTextSubTitle, DTextTitle } from "./DText";
 
 export const DAppLogo = ({ logo, text, size }: TProps) => {
   return (
     <DLayoutCol>
       <Image
         style={{
-          width: size || 80,
-          height: size || 80,
+          width: 100,
+          height: 100,
           backgroundColor: "transparent",
           alignSelf: "center",
-          marginBottom: 0,
+          marginBottom: 40,
         }}
         source={logo}
       />
-      {text && <DTextSubTitle>{text || "your app"}</DTextSubTitle>}
+      <DTextTitle style={{ color: "white", textAlign: "center", fontSize: 40 }}>{STRINGS.APP_NAME}</DTextTitle>
     </DLayoutCol>
   );
 };
