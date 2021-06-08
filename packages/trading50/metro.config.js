4c20a64f10e8ee5ed6b748e09fd4005d7a35baf0
Dipankar Dutta <dip@fb.com>
2021-06-08 01:39:42 +0100

    automated chkin

--- packages/trading50/metro.config.js
+++ packages/trading50/metro.config.js
@@ -1,5 +1,23 @@
+// Export External module..
+// React native doesnt support npm link https://medium.com/@slavik_210/symlinks-on-react-native-ae73ed63e4a7
+// So this is a workaround.
+
+const path = require("path");
+const extraNodeModules = {
+  core: path.resolve(__dirname + "/../core/"),
+  // add here a new module.
+};
+const watchFolders = [
+  path.resolve(__dirname + "/../core/"),
+  // add here the new module
+];
+
 module.exports = {
   transformer: {
-    assetPlugins: ['expo-asset/tools/hashAssetFiles'],
+    assetPlugins: ["expo-asset/tools/hashAssetFiles"],
   },
+  resolver: {
+    extraNodeModules,
+  },
+  watchFolders,
 };
--- packages/trading50/src/screens/CONST.ts
+++ packages/trading50/src/screens/CONST.ts
@@ -1,7 +1,9 @@
 export const CACHE_KEY_MARKET = "CACHE_KEY_MARKET";
+import { sum } from "core";
 export const CACHE_KEY_SUMMARY = "CACHE_KEY_SUMMARY";
 export const CACHE_KEY_POSITION = "CACHE_KEY_POSITION";
 
 export const SIMPLESTORE_ENDPOINT = "https://api.grodok.com";
 export const PRO_TRADING_SERVER = "https://dev.api.grodok.com:5000";
+console.log(sum(10000, 30));
 //export const PRO_TRADING_SERVER = "http://10.0.2.2:5000"; // 10.0.2.2 is the host machine IP
