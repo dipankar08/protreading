f19aaa6bc6af762bca17dbc14ba5579835dd2560
Dipankar Dutta <dip@fb.com>
2021-06-08 01:40:09 +0100

    automated chkin

--- packages/trading50/metro.config.js
+++ packages/trading50/metro.config.js
@@ -1,6 +1,7 @@
 // Export External module..
 // React native doesnt support npm link https://medium.com/@slavik_210/symlinks-on-react-native-ae73ed63e4a7
 // So this is a workaround.
+// No need to use NPM link
 
 const path = require("path");
 const extraNodeModules = {
@@ -9,7 +10,7 @@ const extraNodeModules = {
 };
 const watchFolders = [
   path.resolve(__dirname + "/../core/"),
-  // add here the new module
+  // add here the new module ss
 ];
 
 module.exports = {
