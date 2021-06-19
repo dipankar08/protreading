e45cfdad275aa3912b06d3801c1f5c0ffcf776c2
Dipankar Dutta <dip@fb.com>
2021-06-19 01:58:43 +0100

    automated chkin

--- backend/start_celery.sh
+++ backend/start_celery.sh
@@ -1,3 +1,6 @@
+# Kill all celery process
+ps -ef | grep celery | awk '{print $2}' | xargs kill -9
+
 # Clear older pending task
 pipenv run celery  -A myapp.worker.celery purge -f
 #pipenv run celery -A myapp.worker.celery worker
